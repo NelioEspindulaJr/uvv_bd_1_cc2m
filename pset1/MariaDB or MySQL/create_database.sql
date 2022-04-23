9f1f6a118c42ee60f10e80983385039d3fe1e36f
Nélio Espíndula Junior <nelioespindula.junior@gmail.com>
2022-04-22 21:20:49 -0300

    Correção de alguns detalhes no PostgreSQL. Inicio do desenvolvimento de banco de dados MariaDB/MySQL

---
+++ pset1/MariaDB or MySQL/create_database.sql
@@ -0,0 +1,6 @@
+/*
+No banco de dados MySQL não existem Schemas e Databases, pois eles agem como um só.
+Por isso, decidi criar o banco de dados `elmasri`, ao invés de `uvv`
+*/
+CREATE DATABASE `elmasri` 
+COLLATE 'latin1_general_ci'
--- pset1/MariaDB or MySQL/insert_data.sql
+++ pset1/MariaDB or MySQL/insert_data.sql
@@ -1,28 +1,28 @@
-
 INSERT INTO elmasri.funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
 VALUES
-    ('Jorge', 'E', 'Brito', '88866555576','10-11-1937', 'Rua do Horto, 35, Sao Paulo, SP', 'M', 55000, NULL, 1);
+    ('Jorge', 'E', 'Brito', '88866555576','1937-11-10', 'Rua do Horto, 35, Sao Paulo, SP', 'M', 55000, NULL, 1);
 
 
 -- DEPOIS DE DEFINIR JORGE, INSERI OS OUTROS DOIS GERENTES UTILIZANDO O VALOR PADRÃO '1' DE numero_departamento 
 INSERT INTO elmasri.funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
 VALUES
-    ('Fernando', 'T', 'Wong', '33344555587','08-12-1955', 'Rua da Lapa, 34, Sao Paulo, SP', 'M', 40000, '88866555576', 1),
-    ('Jennifer', 'S', 'Souza', '98765432168','20-06-1941', 'Av. Arthur de Lima, 54, Santo Andre, SP', 'F', 43000, '88866555576', 1);
+    ('Fernando', 'T', 'Wong', '33344555587','1995-12-08', 'Rua da Lapa, 34, Sao Paulo, SP', 'M', 40000, '88866555576', 1),
+    ('Jennifer', 'S', 'Souza', '98765432168','1941-06-20', 'Av. Arthur de Lima, 54, Santo Andre, SP', 'F', 43000, '88866555576', 1);
 
-UPDATE elmasri.departamento SET cpf_gerente = '88866555576' WHERE numero_departamento = 1;
-UPDATE elmasri.departamento SET cpf_gerente = '98765432168' WHERE numero_departamento = 4;
-UPDATE elmasri.departamento SET cpf_gerente = '33344555587' WHERE numero_departamento = 5;
-
+/* 
+Criacao da Fk(foreign key) da tabela departamento referenciando a tabela funcionario.
+Foi criada apenas agora, para evitar problemas de relacionamento entre tais tabelas (incompatibilidade nas relacoes).
+*/
+ALTER TABLE elmasri.departamento ADD CONSTRAINT funcionario_departamento_fk FOREIGN KEY (cpf_gerente) REFERENCES elmasri.funcionario (cpf) ON UPDATE NO ACTION;
 
 -- DADOS DOS DEMAIS FUNCIONARIOS INSERIDOS.
 INSERT INTO elmasri.funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) 
 VALUES
-    ('Joao', 'B', 'Silva', '12345678966', '09-01-1965', 'Rua das Flores, 751, Sao Paulo, SP', 'M', 30000, '33344555587', 5),
-    ('Alice', 'J', 'Zelaya', '99988777767', '19-01-1968', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168', 4),
-    ('Ronaldo', 'K', 'Lima', '66688444476', '15-09-1962', 'Rua Reboucas, 65, Piracicaba, SP', 'M', 38000, '33344555587', 5),
-    ('Joice', 'A', 'Leite', '45345345376', '31-07-1972', 'Av. Lucas Obes, 74, Sao Paulo, SP','F', 25000, '33344555587', 5),
-    ('Andre', 'V', 'Pereira', '98798798733', '29-03-1969', 'Rua Timbira, 35, Sao Paulo, SP', 'M', 25000, '98765432168', 4);
+    ('Joao', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, Sao Paulo, SP', 'M', 30000, '33344555587', 5),
+    ('Alice', 'J', 'Zelaya', '99988777767', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168', 4),
+    ('Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua Reboucas, 65, Piracicaba, SP', 'M', 38000, '33344555587', 5),
+    ('Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes, 74, Sao Paulo, SP','F', 25000, '33344555587', 5),
+    ('Andre', 'V', 'Pereira', '98798798733', '1969-03-29', 'Rua Timbira, 35, Sao Paulo, SP', 'M', 25000, '98765432168', 4);
 
 -- DADOS DA TABELA DE LOCALIZACOES DE DEPARTAMENTO
 INSERT INTO elmasri.localizacoes_departamento (numero_departamento, local)
@@ -43,16 +43,16 @@ VALUES
     ('Reorganizacao', 20, 'Sao Paulo', 1),
     ('NovosBeneficios', 30, 'Maua', 4);
 
---DADOS TABELA DEPENDENTE
+-- DADOS TABELA DEPENDENTE
 INSERT INTO elmasri.dependente (cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
 VALUES
-    ('33344555587', 'Alicia', 'F', '05-04-1986', 'Filha'),
-    ('33344555587', 'Tiago', 'M', '25-10-1983', 'Filho'),
-    ('33344555587', 'Janaina', 'F', '03-05-1958', 'Esposa'),
-    ('98765432168', 'Antonio', 'M', '28-02-1942', 'Marido'),
-    ('12345678966', 'Michael', 'M', '04-01-1988', 'Filho'),
-    ('12345678966', 'Alicia', 'F', '30-12-1988', 'Filha'),
-    ('12345678966', 'Elizabeth', 'F', '05-05-1967', 'Esposa');
+    ('33344555587', 'Alicia', 'F', '1986-04-05', 'Filha'),
+    ('33344555587', 'Tiago', 'M', '1983-10-25', 'Filho'),
+    ('33344555587', 'Janaina', 'F', '1958-05-03', 'Esposa'),
+    ('98765432168', 'Antonio', 'M', '1942-02-28', 'Marido'),
+    ('12345678966', 'Michael', 'M', '1988-01-04', 'Filho'),
+    ('12345678966', 'Alicia', 'F', '1988-12-30', 'Filha'),
+    ('12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
 
 -- DADOS TABELA TRABALHA EM
 INSERT INTO elmasri.trabalha_em (cpf_funcionario, numero_projeto, horas)
